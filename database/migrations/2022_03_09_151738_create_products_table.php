e5d6d099d39d1a01b9cef63202e8cb7ea3da976c
Victor Alando <124558542+WebXpertAlando@users.noreply.github.com>
2023-10-26 10:56:32 +0300

    Add files via upload

---
+++ database/migrations/2022_03_09_151738_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->id();
+            $table->string("barcode");
+            $table->string("description");
+            $table->decimal("purchase_price", 9, 2);
+            $table->decimal("sales_price", 9, 2);
+            $table->decimal("existence", 9, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
